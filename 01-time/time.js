512c32f7ae5c74bba645987b43e8d2d1e61b31ed
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-02-13 21:28:40 +0100

    Removed test

--- 01-time/time.js
+++ 01-time/time.js
@@ -1,16 +1,5 @@
 'use strict';
 
-/*
- * @return {String} - Formatted string with current day
- */
-module.exports.getDate = function () {
-  const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wenesday', 'Thursday', 'Friday', 'Saturday'],
-        today = new Date();
-  var day = today.getDay();
-  console.log('day: ', day);
-  return 'Today is : ' + weekDays[day] + 1;
-};
-
 module.exports.checkFormattedTime = function (time) {
   const pattern = /[0-9]\s[a-zA-Z]{2}\s:\s[0-5][0-9]\s:\s[0-5][0-9]/;
 
