8ec5d97f23217d1ff81a51ba32e9f0736a304700
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-05-08 17:43:00 +0200

    Fixed password checker. Not finished yet

--- 04-password-check/password-checker.js
+++ 04-password-check/password-checker.js
@@ -2,52 +2,22 @@
 
 'use strict';
 
-module.exports.checkPassword = function(pass) {
+module.exports.checkLowerCase = function(pass) {
   let result;
-  // const lowerCase = /[a-z]+[A-Z]+[0-9]+[$#@]/;
-  const lowerCase = /[a-z]+[0-9]$/;
-  // const upperCase = /[A-Z]/;
-  // const number = /[0-9]/;
+  const containsLowerCase = /[a-z]{1,}/;
 
-  result = lowerCase.test(pass) && pass.length >= 6 && pass.length <= 16;
-
-  // let newResult = result.filter(checkFalse);
-
-  // function checkFalse(value) {
-  //   console.log('value === false: ' + value === false + '\n');
-  //   return value === false;
-  // }
-  // console.log("newResult: ", newResult.length);
+  result = containsLowerCase.test(pass);
   return result;
 };
 
-// 'use strict';
+module.exports.checkNumber = function(pass) {
+  let result;
+  const containsNumber = /[0-9]{1,}/;
 
-// module.exports.checkPassword = function(pass) {
-//   const regExp = /[a-z]+[A-Z]+[0-9]+[$#@]/,
-//         lowerCase = /[a-z]/,
-//         camelcase = /[A-Z]/,
-//         num = /[0-9]/,
-//         character = /[$#@]/;
-//   let result;
+  result = containsNumber.test(pass);
+  return result;
+};
 
-//   this.checkLowerCase = function(char) {
-//    return lowerCase.test(char);
-//   };
-//   this.checkUpperCase = function(char) {
-//     return camelCase.test(char)
-//   };
-//   this.isNumber = function(char) {
-//     return num.test(char)
-//   };
-
-//   this.isCharacter = function(char) {
-//     return character.test(char)
-//   }
-//   result = regExp.test(pass) && pass.length >= 6 && pass.length <= 16;
-
-//   console.log('regExp.test(pass): ', regExp.test(pass));
-
-//   console.log('result: ', result);
-//   return result;
-// };
+module.exports.checkLength = function(pass) {
+  return pass.length >= 6 && pass.length <= 16;
+};
--- test/password-validation.js
+++ test/password-validation.js
@@ -11,15 +11,19 @@ describe('Password Checker', function(){
   describe('Write a program to check the validity of a password', function(){
     it('should contains at least 1 letter between [a-z]', function(){
       let pass = 'Bz8E#d';
-      expect(PassValidator.checkPassword(pass)).to.be.true;
+      expect(PassValidator.checkLowerCase(pass)).to.be.true;
     });
     it('should contains at least 1 number between [0-9]', function(){
       let pass = 'Bz8E#d';
-      expect(PassValidator.checkPassword(pass)).to.be.true;
+      expect(PassValidator.checkNumber(pass)).to.be.true;
     });
-    it('should contains at least 1 character from [$#@]', function(){
-      let pass = 'Bz8E#d';
-      expect(PassValidator.checkPassword(pass)).to.be.true;
-    });
+    it('should be at least a length of 6 chareacter as minumum and 16 as maximun', function() {
+        let pass = 'Bz8E#d';
+        expect(PassValidator.checkLength(pass)).to.be.true;
+    })
+    // it('should contains at least 1 character from [$#@]', function(){
+    //   let pass = 'Bz8E$#@d';
+    //   expect(PassValidator.checkPassword(pass)).to.be.true;
+    // });
   });
 });
