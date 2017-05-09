cbd1cb5e5266fe6903c8c98f5d213992f99c68c1
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-05-09 09:55:54 +0200

    Added all test for basic numbers and strings tests

--- 00_generic-exercises/numbers-strings.js
+++ 00_generic-exercises/numbers-strings.js
@@ -1,6 +1,9 @@
 'use strict';
 
 module.exports.palindrome = function (str) {
-  // Good luck!
-  return true;
+  const sanitizedStr = str.replace(/\s/g,'').replace(/[\W_]+/g, '').toLowerCase();
+  const reversedStr = sanitizedStr.split('').reverse().join('');
+  console.log('sanitizedStr: ', sanitizedStr);
+  console.log('reversedStr: ', reversedStr);
+  return sanitizedStr === reversedStr;
 }
