2001b140b3c4f93f5dec65e149e37c129a92c739
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-05-11 02:37:23 +0200

    Removed unnecesary logs

--- 00_generic-exercises/00-numbers-strings.js
+++ 00_generic-exercises/00-numbers-strings.js
@@ -3,7 +3,5 @@
 module.exports.palindrome = function (str) {
   const sanitizedStr = str.replace(/\s/g,'').replace(/[\W_]+/g, '').toLowerCase();
   const reversedStr = sanitizedStr.split('').reverse().join('');
-  console.log('sanitizedStr: ', sanitizedStr);
-  console.log('reversedStr: ', reversedStr);
   return sanitizedStr === reversedStr;
 }
