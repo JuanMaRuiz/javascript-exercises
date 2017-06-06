65a7bc090dac874390d737fde4efd40c87310cf3
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-06-06 08:51:45 +0200

    Fixed error in array test

--- test/03-largest-numbers-in-arrays-test.js
+++ test/03-largest-numbers-in-arrays-test.js
@@ -13,8 +13,10 @@ describe('Return Largest Numbers in Arrays', function() {
     });
     it('largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27,5,39,1001]', function() {
         const givenArr = [[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]];
-        const expectedValue = [27,5,39,1001];
-        console.log("typeof expectedValue: ", typeof expectedValue);
-        expect(largestNumInArr.largestOfFour(givenArr)).to.deeply.equal(expectedValue); // Change deeply for to.equal for checking type of return
+        expect(largestNumInArr.largestOfFour(givenArr)).to.deep.equal([27,5,39,1001]); // Change deep for to.equal for checking type of return
+    });
+    it('largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [27,5,39,1001]', function() {
+        const givenArr = [[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]];
+        expect(largestNumInArr.largestOfFour(givenArr)).to.deep.equal([9, 35, 97, 1000000]); // Change deep for to.equal for checking type of return
     });
 });
