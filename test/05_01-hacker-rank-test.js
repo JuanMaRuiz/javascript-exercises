2dfe5ef4373a394ad73d8278c773856dd204de20
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-08-20 11:35:55 +0200

    Removed iojs from travis and added arrow function to tests

--- test/05_01-hacker-rank-test.js
+++ test/05_01-hacker-rank-test.js
@@ -6,22 +6,22 @@ const expect = chai.expect;
 
 const maxNumberOfToys = require('./../05_hacker-rank/01_exercise');
 
-describe('Mark and Toys', function () {
-    describe("Write a JavaScript program to test how many toys can buy for a given amount", function () {
-        it("should return 4 toys", function () {
+describe('Mark and Toys', () => {
+    describe("Write a JavaScript program to test how many toys can buy for a given amount", () => {
+        it("should return 4 toys", () => {
             const prices = ["1", "12", "5", "111", "200", "1000", "10"];
             const numOfToys = 7;
             const amount = 50;
             expect(maxNumberOfToys.maxiumumToys(prices, amount)).to.be.equal(4);
         });
-        it("should return 4 toys", function () {
+        it("should return 4 toys", () => {
             const prices = ["1000", "12", "5", "111", "200", "1", "10"];
             const numOfToys = 7;
             const amount = 50;
             expect(maxNumberOfToys.maxiumumToys(prices, amount)).to.be.equal(4);
         });
 
-        it("should return 2 toys", function () {
+        it("should return 2 toys", () => {
             const prices = ["1000", "102", "35", "111", "200", "1", "1230"];
             const numOfToys = 7;
             const amount = 50;
