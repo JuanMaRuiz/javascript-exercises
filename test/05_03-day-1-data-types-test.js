eea957a9caf0b36b690fc383a771d2cb8ee41cdd
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-09-25 23:10:15 +0200

    Added day 1 data types hacker rank

---
+++ test/05_03-day-1-data-types-test.js
@@ -0,0 +1,11 @@
+'use strict';
+// Core dependencies
+const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+
+const dataTypes = require('../05_hacker-rank/03-day-1-data-types');
+
+describe('Day 1 - Data Types', () => {
+
+});
