f5c32103729113ed2f4084eae267e30516a49612
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-09-26 18:04:08 +0200

    Added new exercise checking strings

---
+++ 05_hacker-rank/03-sherlock.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports.checkStr = function (str) {
+    const regExp = /[a-z]/;
+
+    return "hello";
+}
---
+++ test/05_03-sherlock.js
@@ -0,0 +1,16 @@
+'use strict';
+// Core dependencies
+const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+
+const sherlock = require('../05_hacker-rank/03-sherlock');
+
+describe('Sherlock and the Valid String', () => {
+    describe('Given string "aabbcd" has a valid frecuency', () => {
+        it('should return "NO"', () => {
+            const str = "aabbcd";
+            expect(sherlock.checkStr(str)).to.equal('NO');
+        });
+    });
+});
